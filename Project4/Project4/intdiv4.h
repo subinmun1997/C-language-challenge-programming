b2640e23360551e6df15038dc7581a2067557820
subinmun1997 <qzxy812@gmail.com>
2020-06-13 18:33:21 +0900

    MultiFilePrj

---
+++ Project4/Project4/intdiv4.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef __INTDIV4_H__
+#define __INTDIV4_H__
+
+#include "stdiv2.h"
+Div IntDiv(int num1, int num2);
+
+#endif
---
+++ Project4/Project4/stdiv2.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef __STDIV2_H__
+#define __STDIV2_H__
+
+typedef struct div {
+	int quotient;
+	int remainder;
+} Div;
+
+#endif
